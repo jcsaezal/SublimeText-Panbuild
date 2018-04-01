58445ddc5ff801120814e9853696d803d22b2194
Juan Carlos Saez <jcsaezal@ucm.es>
2018-04-01 21:14:10 +0200

    Initial commit

---
+++ Panbuild.py
@@ -0,0 +1,196 @@
+# Copyright (c) 2017 Juan Carlos Saez
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sublime
+import sublime_plugin
+import pprint
+import re
+import subprocess
+import tempfile
+import os
+import errno
+import threading
+
+
+class PanbuildSettings(threading.Thread):
+    def __init__(self,targets,outfiles,commands,working_directory):
+        self.targets=targets
+        self.outfiles=outfiles
+        self.commands=commands
+        self.dirname=working_directory
+        self.target_number=-1
+        self.window=-1
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.run_panbuild(self.target_number)
+
+    def run_panbuild(self,target_number):
+        if target_number<0 or target_number>=len(self.targets):
+            return 1
+
+        active_target=self.targets[target_number]
+        cmd=self.commands[active_target]
+        outfile=self.outfiles[active_target]
+
+        # write panbuild command to console
+        print('Running Command: '+' '.join(cmd))
+    # run command
+        process = subprocess.Popen(
+            cmd, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE, cwd=self.dirname)
+        result, error = process.communicate() #contents.encode('utf-8'))
+        exitcode = process.returncode
+
+        # handle pandoc errors
+        if exitcode!=0:
+            sublime.error_message('\n\n'.join([
+                'Error when running:',
+                ' '.join(cmd),
+                error.decode('utf-8').strip()]))
+            return
+
+        # if write to file, open
+        if outfile is not None:
+            output_path=os.path.join(self.dirname, outfile)
+            try:
+                if sublime.platform() == 'osx':
+                    subprocess.call(["open", output_path])
+                elif sublime.platform() == 'windows':
+                    os.startfile(output_path)
+                elif os.name == 'posix':
+                    subprocess.call(('xdg-open', output_path))
+            except:
+                sublime.message_dialog('Wrote to file ' + output_path)
+
+            sublime.set_timeout(lambda: self.window.run_command('show_panel', {"panel": "console","toggle": True})   , 100)        
+            return 0
+
+        return 1
+
+class PromptPanbuildCommand(sublime_plugin.WindowCommand):
+
+    '''Defines the plugin command palette item.
+
+    @see Default.sublime-commands'''
+
+    options = []
+
+    def retrieve_targets(self,build_file,build_file_full,working_directory,inSource=False):
+        if not os.path.isfile(build_file_full):
+            return (errno.ENOENT,None)
+
+        if inSource:
+            cmd=["panbuild","-Lo","-f",build_file]  
+            basecmd=["panbuild","-f",build_file]
+        else:
+            cmd=["panbuild","-Lo"]  
+            basecmd=["panbuild"]
+
+        process = subprocess.Popen(
+                cmd, shell=False, #stdin=subprocess.PIPE, 
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE, cwd=working_directory)
+        result, error = process.communicate()
+        exitcode = process.returncode
+
+        # handle pandoc errors
+        if exitcode and error:
+            #print('Error when running:',join(cmd),error.decode('utf-8').strip())
+            return (errno.ENOENT,None)
+        else:
+            lines=result.splitlines()
+            targets=[]
+            outfiles={}
+            commands={}
+            for line in lines:
+                tokens=line.strip().split(b':')
+                target=tokens[0].decode("utf-8")
+                
+                if len(tokens)!=2 or tokens[1].strip() == "":
+                    continue
+
+                outfile=tokens[1].strip().decode("utf-8")
+
+                targets.append(target)
+                outfiles[target]=outfile
+                ##Create CMD
+                cool_cmd=list(basecmd)
+                cool_cmd.append(target)
+                commands[target]=cool_cmd
+
+            return (0,PanbuildSettings(targets,outfiles,commands,working_directory))
+
+    def run(self):
+        if self.window.active_view():
+            self.window.show_quick_panel(
+                self.get_build_targets(),
+                self.build)
+
+    def get_build_targets(self):
+        '''Generates a ranked list of available transformations.'''
+        view = self.window.active_view()
+
+        fname=view.file_name()
+
+        fpath=os.path.abspath(fname)
+
+        dirname, filename=os.path.split(fpath)
+
+        ## Search for build YAML
+        buildfile=os.path.join(dirname, "build.yaml")
+
+        (code,self.build_settings)=self.retrieve_targets("build.yaml",buildfile,dirname,inSource=False)
+
+        if code==0: ## Buildfile was found
+            print("Found build file at %s" % buildfile)
+            return self.build_settings.targets
+
+        ## try to get it from there 
+        (code,self.build_settings)=self.retrieve_targets(fname,fpath,dirname,inSource=True)
+
+        if code==0: 
+            print("Found build settings in file %s" % fpath)
+            return self.build_settings.targets
+        else:
+            sublime.error_message("No build rules found for the file")
+            return ["Failed"]
+
+
+    def build(self, i):
+        if i == -1:
+            return
+        #view = self.window.active_view()
+        self.window.run_command('show_panel', {"panel": "console"})
+        self.build_settings.window=self.window
+        version=sublime.version()
+        if version[0]=='2':
+            self.build_settings.target_number=i
+            self.build_settings.start()
+            # Dangerous...
+            #sublime.set_timeout(lambda: self.build_settings.join(), 2000)
+        else:
+            sublime.set_timeout_async(lambda: self.build_settings.run_panbuild(i), 1)
+
+        return 
---
+++ messages/install.md
@@ -0,0 +1,8 @@
+For this plugin to work, you will need the Pandoc and Panbuild tools to be installed on the system. Moreover the executable files for both tools must be in your PATH.
+
+* http://johnmacfarlane.net/pandoc/installing.html
+* https://github.com/jcsaezal/panbuild
+
+Visit the project page for more info:
+
+https://github.com/jcsaezal/SublimeText-Panbuild
